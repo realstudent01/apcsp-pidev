f20e2f27c63d771fd1cea1fd34ad1087f3eb4ea5
Joseph Harrington <josephdanielsd@gmail.com>
2018-05-30 22:54:24 +0000

    work

--- inputAreaOfCircle.c
+++ inputAreaOfCircle.c
@@ -6,16 +6,36 @@ int main( int argc, char* argv[])
  int r2;
  int i;
  float r;
- if ( *argv[1] > *argv[2])   //argc[1] is a and argc[2] is b
+// if ( argv[0] > argv[1])   //argc[1] is a and argc[2] is b
+// {
+//  r1 = argv[1];
+//  r2 = argv[0];
+// }
+// if ( argv[0] < argv[1])
+// {
+//  r1 = argv[0];
+//  r2 = argv[1];
+// }
+
+int input1;
+int input2;
+
+if ( argc == 0)
  {
-  r1 = *argv[2];
-  r2 = *argv[1];
+  printf("enter the integer that you want to be the lower range of the area of a circle output\n");
+  fgets(input, 256, stdin);
+  sscanf(input1, "%s", r1);
+  printf("enter the integer that you want to be the upper range of the area of a circle output\n");
+  fgets(input2, 256, stdin);
+  sscanf(input, "%s", r2);
  }
- else
- {
-  r1 = *argv[1];
-  r2 = *argv[2];
- }
+
+
+
+
+
+
+
 
  for ( i = r1; i > r2; i++)
  {
