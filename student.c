4fb23fb90b27b47d3fd5e7c64f23d416f6e77c2f
Joseph Harrington <josephdanielsd@gmail.com>
2018-05-31 16:14:09 +0000

    student fixed

--- student.c
+++ student.c
@@ -1,46 +1,6 @@
 #include <stdio.h>
-#include <string.h>
 
 int main()
 {
- int i;
- int num = 97;
- char c = 'a';
- char alphabet[27];
 
- for ( i = 0; i < 26; i++)
- {
-  alphabet[i] = c;
-//  printf("%s\n", alphabet);
-  c++;
- }
- alphabet[26] = '\0';
- printf("%s\n", alphabet);
-
- char alphabet2 [27] = "abcdefghijklmnopqrstuvwxyz\0";
- printf("%s\n", alphabet2);
-
- if (strcmp(alphabet, alphabet2) == 0 )
- {
-  printf("the strings are identical\n");
- }
- else
- {
-  printf("the strings are different\n");
- }
-
- for ( i = 0; i < 26; i++)
- {
-  alphabet[i] = alphabet[i] - 32;
- }
- printf("%s\n", alphabet);
-
- if (strcmp(alphabet, alphabet2) == 0 )
- {
-  printf("the strings are identical\n");
- }
- else
- {
-  printf("the strings are different\n");
- }
 }
