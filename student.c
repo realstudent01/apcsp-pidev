a4f4ff00f8af0b5fe4b4f7b064abf5d9b256ee57
Joseph Harrington <josephdanielsd@gmail.com>
2018-05-31 16:29:37 +0000

    structs

--- student.c
+++ student.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+struct Student
+{
+ char firstName[50];
+ char lastName[50];
+ char age[2];
+ char studentID[50];
+};
+
+void printStudent( int argc, char* argv[])// struct Student* student)
+{
+ printf("student 1 name is %s %s\n", student1.firstName, student1.lastName);
+}
+
 int main()
 {
+ struct Student student1;
+ struct Student student2;
+
+ printStudent(student1);
+ printStudent(student2);
 
 }
