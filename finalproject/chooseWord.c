a43cb46a68a05aba269379a7ade28668fe8fd560
Joseph Harrington <josephdanielsd@gmail.com>
2018-06-04 16:26:37 +0000

    day 1

---
+++ finalproject/chooseWord.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+
+
+int chooseWord()
+{
+
+  char wordArr[11][25] = { "soldier", "acquaintance", "capture", "pair", "bedroom", "beach", "support", "ice", "sensitivity", "fabricate", "producer" };
+  //int i = rand() % (11 + 1);
+
+ // char arr[3] = {"soldier", "pair", "bedroom"};
+ // char w = arr[2];
+
+// char word = "wordArr[i]";
+// printf("%s\n", w);
+
+
+
+
+}
---
+++ finalproject/hangman.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+
+char getCharFromUser()
+{
+	char input[256];
+	char letter;
+	while (1)
+	{
+		printf("Guess a letter\n");
+		fgets(input, 256, stdin);
+		if (sscanf(input, "%c", &letter) == 1)
+		{
+			break;
+		}
+	}
+	return letter;
+}
+
+int main()
+{
+	char letter;
+	int word;
+
+	letter = getCharFromUser();
+}
