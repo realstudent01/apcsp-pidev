074f29bd526b70f4436bcdd7c352cf1e9e9db573
Joseph Harrington <josephdanielsd@gmail.com>
2018-06-05 18:07:14 +0000

    day two updates

--- finalproject/chooseWord.c
+++ finalproject/chooseWord.c
@@ -6,14 +6,17 @@
 
 int main()
 {
- char wordArr[11][25] = { "soldier\0", "acquaintance\0", "capture\0", "pair\0", "bedroom\0", "beach\0", "support\0", "ice\0", "sensitivity\0", "fabricate\0","producer\0" };
+ char wordArr[][25] = { "soldier\0", "acquaintance\0", "capture\0", "pair\0", "bedroom\0", "beach\0", "support\0", "ice\0", "sensitivity\0", "fabricate\0","producer\0", "awkward\0", "bagpipes\0", "dwarves\0", "zombie\0", "quizzes\0", "queue\0", "weekend\0" };
  srand(time(0));
- int n = rand() % (11);
+
+ size_t length = sizeof(wordArr)/sizeof(wordArr[0]);
+ int n = rand() % (length);
  int l = strlen(wordArr[n]);
  char wordString[l+1];
  int i;
  char a;
 
+// printf("%d\n", length);
  for ( i = 0; i < l; i++)
  {
   a = wordArr[n][i];
