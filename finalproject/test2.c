fe5211dd2f1bd51d79aa9490fd031e4fba2aa7e3
Joseph Harrington <josephdanielsd@gmail.com>
2018-06-06 21:53:51 +0000

    day 3 4th period

--- finalproject/test2.c
+++ finalproject/test2.c
@@ -6,6 +6,9 @@
 int main()
 {
  char wordArr[][25] = { "soldier\0", "acquaintance\0", "capture\0", "pair\0", "bedroom\0", "beach\0", "support\0", "ice\0", "sensitivity\0", "fabricate\0","producer\0", "awkward\0", "bagpipes\0", "dwarves\0", "zombie\0", "quizzes\0", "queue\0", "weekend\0" };
+ char unguessedAlphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+// char incorrectguesses[];
+
  srand(time(NULL));
 
  size_t length = sizeof(wordArr)/sizeof(wordArr[0]);
@@ -15,7 +18,6 @@ int main()
  int i;
  char a;
 
-// printf("%d\n", length);
  for ( i = 0; i < l; i++)
  {
   a = wordArr[n][i];
@@ -23,39 +25,57 @@ int main()
  }
 
  wordString[l] = '\0';
-
  printf("%s\n", wordString);
 
-// char getCharFromUser()
-	char input[256];
-	char letter;
-	while (1)
-	{
-		printf("Guess a letter\n");
-		fgets(input, 256, stdin);
-		if (sscanf(input, "%c", &letter) == 1)
-		{
-			break;
-		}
-	}
-
-
-
- printf("%s     %c\n", wordString, letter);
-
-char displayword[l];
-
-
+ char displaywordB[l];    // display word before
  for ( i = 0; i < l; i++)
  {
-  if (wordString[i] == letter)
+  displaywordB[i] = '_';
+ }
+ char displaywordA[l];    // display word after
+
+while ( strcmp(wordString, displaywordA) == 1 )
+{
+ char input[256];
+ char letter;
+  while (1)
   {
-   displayword[i] = letter;
+   printf("Guess a letter\n");
+   fgets(input, 256, stdin);
+   if (sscanf(input, "%c", &letter) == 1)
+    {
+     continue;		// I think tihs is the problem because it is breaking out of both while loops
+    }
+  }
+
+   for ( i = 0; i < l; i++)
+   {
+    if ( displaywordB[i] == '_' )
+    {
+     if (wordString[i] == letter)
+     {
+      displaywordA[i] = letter;
+     }
+     else
+     {
+      displaywordA[i] = '_';
+     }
+    }
+   }
+
+  if (strcmp(displaywordB, displaywordA) == 1) //checks if the before and after are the same and if they are then the guess was wrong, if they're not the same then the guess was right
+  {		// THING TO ADD: use ascii table and some math to go to where in the unused alphabet string the correct guess was in order to move that letter to the already guessed string
+   printf("incorrect\n");
   }
   else
   {
-   displayword[i] = '_';
+   printf("correct\n");
+  }
+ printf("%s\n", displaywordA);
+
+  for (i = 0; i < l; i++)		//sets display word before to the display word after from the last round so that it is ready for next round
+  {
+   displaywordB[i] = displaywordA[i];
   }
  }
- printf("%s\n", displayword);
 }
